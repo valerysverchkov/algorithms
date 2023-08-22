3c1f1872610062fd6faeabefb5178f69eeacf671
vsverchkov <valerysverchkov@gmail.com>
2023-08-22 10:40:40 +0300

    Added Recursive Search in a Binary Search Tree

---
+++ leet-code/search-in-a-binary-search-tree.go
@@ -0,0 +1,27 @@
+package leet_code
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+
+	return searchBST(root.Left, val)
+}
